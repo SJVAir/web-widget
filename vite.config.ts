fad420562d9426f52135c6382d6d3b02e447febb
Alexander McCormick <alexandermccormick@protonmail.com>
2023-07-27 16:52:43 -0700

    corrects base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,8 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/static/widget",
   plugins: [vue()],
-  //css: {
-  //  preprocessorOptions: {
-  //    scss: {
-  //      additionalData: `@use "src/styles/utils";`
-  //    },
-  //  }
-  //},
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
