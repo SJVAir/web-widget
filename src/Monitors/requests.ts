ad777f54f5941daa7fbb6708d49d535808717c0d
Alexander McCormick <alexandermccormick@protonmail.com>
2023-08-01 17:02:30 -0700

    corrects generation of base url for requests

--- src/Monitors/requests.ts
+++ src/Monitors/requests.ts
@@ -2,7 +2,7 @@ import type { MonitorData } from "./index.d";
 
 export const getUrl = (endpoint: string): string => {
   const rootURL = (import.meta.env.PROD)
-    ? new URL(import.meta.url).origin
+    ? window.location.origin
     : import.meta.env.VITE_DEV_URL;
 
     return `${ rootURL }/api/1.0/${ endpoint }`;
