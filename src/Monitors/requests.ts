ba36f92079c9455c5a0bd99fea56072aeb542545
Alexander McCormick <alexandermccormick@protonmail.com>
2025-08-25 19:20:43 -0700

    Updates requests to use @sjvair/sdk

--- src/Monitors/requests.ts
+++ src/Monitors/requests.ts
@@ -1,54 +1,11 @@
-import type { MonitorData } from "./index.d";
+import { getMonitorDetails, setOrigin, type MonitorDetails } from "@sjvair/sdk";
 
-export const getUrl = (endpoint: string): string => {
-  const rootURL = (import.meta.env.PROD)
-    ? window.location.origin
-    : import.meta.env.VITE_DEV_URL;
+const origin = (import.meta.env.PROD)
+  ? window.location.origin
+  : import.meta.env.VITE_DEV_URL;
 
-    return `${ rootURL }/api/1.0/${ endpoint }`;
-}
+setOrigin(origin);
 
-const errmsg = (msg: string) => `Unable to fetch monitor: ${ msg }`;
-
-export async function fetchMonitorById(id?: string): Promise<MonitorData | undefined> {
-  if (!id) {
-    return Promise.reject(errmsg("No monitor ID provided"));
-  }
-
-  const url = getUrl(`monitors/${ id }/`);
-
-  try {
-    const response = await fetch(url);
-
-    if (response.status === 200) {
-      const { data } = await (response.json() as Promise<{ data: MonitorData }>);
-
-      if (data.latest) {
-
-        if ("pm25" in data.latest && parseInt(data.latest.pm25, 10) < 0) {
-          data.latest.pm25 = "0";
-        }
-
-        if ("pm25_avg_60" in data.latest && parseInt(data.latest.pm25_avg_60, 10) < 0) {
-          data.latest.pm25_avg_60 = "0";
-        }
-
-        return data;
-      }
-    } else {
-      switch(response.status) {
-        case 404:
-          return Promise.reject(errmsg(`No monitor found with ID ${ id }`));
-
-        case 500:
-          return Promise.reject(errmsg(`${ id } is not a valid monitor ID`));
-
-        default:
-          return Promise.reject(errmsg(`Failed to fetch monitor with id ${ id }. Response status: ${ response.status }`))
-      }
-
-    }
-  } catch (err) {
-    return Promise.reject(err);
-  }
+export async function getMonitor(monitorId: string): Promise<MonitorDetails | undefined> {
+  return await getMonitorDetails(monitorId)
 }
