12369eeebd9f66668789c04a2def31298b4b90f3
Alexander McCormick <alexandermccormick@protonmail.com>
2025-08-26 13:53:10 -0700

    Removes references to old widget service

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
-  import { useWidgetService } from "@/WidgetService";
 
-  const { error } = await useWidgetService();
 </script>
 
 <template>
-  <h1>{{ error }}</h1>
+  <h1>There was an error</h1>
 </template>
 
 <style scoped lang="scss">
-  h1 {
-    font-size: 1rem;
-  }
+h1 {
+  font-size: 1rem;
+}
 </style>
