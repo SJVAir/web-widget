ff5f906ec68a09c722d2e2a372130059976fa327
Alexander McCormick <alexandermccormick@protonmail.com>
2025-08-25 19:21:40 -0700

    Updated to use new requests module

--- src/WidgetService.ts
+++ src/WidgetService.ts
@@ -1,20 +1,20 @@
 import { ref } from "vue";
 import { useRouter } from "vue-router";
-import { fetchMonitorById } from "@/Monitors";
+import { type MonitorDetails } from "@sjvair/sdk";
 import { asyncInitializer } from "@/Utils"
 import type { Ref } from "vue";
-import type { MonitorData } from "@/Monitors";
+import { getMonitor } from "./Monitors";
 
 interface WidgetService {
   monitorId: Ref<string | undefined>;
-  monitor: Ref<MonitorData | undefined>;
+  monitor: Ref<MonitorDetails | undefined>;
   error: Ref<string | undefined>;
 
   stopAutoRefresh(): void;
 }
 
 const monitorId = ref<string>();
-const monitor = ref<MonitorData>();
+const monitor = ref<MonitorDetails | undefined>();
 const error = ref<string>();
 
 export const useWidgetService = asyncInitializer<WidgetService>(async (resolve, id: string) => {
@@ -23,7 +23,7 @@ export const useWidgetService = asyncInitializer<WidgetService>(async (resolve, 
   await updateMonitor(id);
 
   const interval = setInterval(async () => await updateMonitor(id), (1000 * 60 * 2));
-  
+
   resolve({
     monitorId,
     monitor,
@@ -35,8 +35,10 @@ export const useWidgetService = asyncInitializer<WidgetService>(async (resolve, 
 async function updateMonitor(id: string) {
   const router = useRouter();
 
-  await fetchMonitorById(id)
-    .then(_monitor => { monitor.value = _monitor; })
+  monitor.value = await getMonitor(id)
+    .then(_monitor => {
+      monitor.value = _monitor;
+    })
     .catch(err => {
       error.value = err;
       router.push({ name: "ErrorVue" });
