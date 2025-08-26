5c40cb5c8fa8104bc7f58ebd4e852ca79696c51b
Alexander McCormick <alexandermccormick@protonmail.com>
2025-08-26 13:52:51 -0700

    Updated to use new WidgetController

--- src/views/MyAir.vue
+++ src/views/MyAir.vue
@@ -1,41 +1,43 @@
 <script setup lang="ts">
-  import { onBeforeUnmount } from "vue";
-  import PMGaugeVue from "@/components/PMGauge.vue";
-  import { useWidgetService } from "@/WidgetService";
+import { onBeforeUnmount } from "vue";
+import PMGaugeVue from "@/components/PMGauge.vue";
+import { WidgetController } from "@/WidgetService";
 
-  const { monitorId } = defineProps<{ monitorId: string }>();
-  const { monitor, stopAutoRefresh } = await useWidgetService(monitorId);
+const widgetCtl = new WidgetController();
 
-  onBeforeUnmount(stopAutoRefresh);
+widgetCtl.updateMonitor();
+widgetCtl.refreshInterval.start();
+
+onBeforeUnmount(() => widgetCtl.refreshInterval.stop());
 </script>
 
 <template>
-  <div v-if="monitor" class="page-content">
+  <div v-if="widgetCtl.monitor.value" class="page-content">
 
     <div class="pmgauge-component">
-      <PMGaugeVue :monitor="monitor"/>
+      <PMGaugeVue :monitor="widgetCtl.monitor.value" />
     </div>
 
   </div>
 </template>
 
 <style scoped lang="scss">
-  .page-content {
-    user-select: none;
+.page-content {
+  user-select: none;
 
-    .pmgauge-component {
-      
-      strong {
-        font-size: 20px;
-        line-height: 26px;
-      }
-    }
+  .pmgauge-component {
 
-    .forecast-component,
-    .guidelines-component {
-      display: flex;
-      justify-content: center;
-      align-items: center;
+    strong {
+      font-size: 20px;
+      line-height: 26px;
     }
   }
+
+  .forecast-component,
+  .guidelines-component {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
 </style>
