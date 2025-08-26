8f3179746cd4cb551aade18ed93ff97cfb7e54bb
Alexander McCormick <alexandermccormick@protonmail.com>
2025-08-26 13:52:34 -0700

    Simplifies widget service

--- src/WidgetService.ts
+++ src/WidgetService.ts
@@ -1,46 +1,30 @@
-import { ref } from "vue";
-import { useRouter } from "vue-router";
-import { type MonitorDetails } from "@sjvair/sdk";
-import { asyncInitializer } from "@/Utils"
-import type { Ref } from "vue";
+import { shallowRef, type ShallowRef } from "vue";
+import type { MonitorDetails } from "@sjvair/sdk";
+import { Singleton } from "@tstk/decorators";
+import { Interval } from "@tstk/utils";
 import { getMonitor } from "./Monitors";
+import { ViewController } from "./views/view-controller";
 
-interface WidgetService {
-  monitorId: Ref<string | undefined>;
-  monitor: Ref<MonitorDetails | undefined>;
-  error: Ref<string | undefined>;
+@Singleton
+export class WidgetController {
+  readonly monitor: ShallowRef<MonitorDetails | undefined> = shallowRef();
+  readonly refreshInterval: Interval = new Interval(() => this.updateMonitor(), 1000 * 60 * 2);
 
-  stopAutoRefresh(): void;
+  private monitorId?: string = window.location.pathname.split("/").pop();
+
+  async updateMonitor() {
+    const viewCtl = new ViewController();
+    if (!this.monitorId) {
+      return viewCtl.gotoError();
+    }
+
+    await getMonitor(this.monitorId)
+      .then(monitor => {
+        this.monitor.value = monitor;
+      })
+      .catch(_ => {
+        viewCtl.gotoError();
+      });
+  }
 }
 
-const monitorId = ref<string>();
-const monitor = ref<MonitorDetails | undefined>();
-const error = ref<string>();
-
-export const useWidgetService = asyncInitializer<WidgetService>(async (resolve, id: string) => {
-  monitorId.value = id;
-
-  await updateMonitor(id);
-
-  const interval = setInterval(async () => await updateMonitor(id), (1000 * 60 * 2));
-
-  resolve({
-    monitorId,
-    monitor,
-    error,
-    stopAutoRefresh: () => clearInterval(interval)
-  });
-});
-
-async function updateMonitor(id: string) {
-  const router = useRouter();
-
-  monitor.value = await getMonitor(id)
-    .then(_monitor => {
-      monitor.value = _monitor;
-    })
-    .catch(err => {
-      error.value = err;
-      router.push({ name: "ErrorVue" });
-    });
-}
